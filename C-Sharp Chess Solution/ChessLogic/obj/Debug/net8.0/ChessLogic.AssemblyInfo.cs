b69c30751d91b3891078fb6e766485093c4c35aa
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2024-03-14 23:53:32 +0800

    changes in background code

--- C-Sharp Chess Solution/ChessLogic/obj/Debug/net8.0/ChessLogic.AssemblyInfo.cs
+++ C-Sharp Chess Solution/ChessLogic/obj/Debug/net8.0/ChessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4085b7013ffe22790f1fac8f551093b06bdefe8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
