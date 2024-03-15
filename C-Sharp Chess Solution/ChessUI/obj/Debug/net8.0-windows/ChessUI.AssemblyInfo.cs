7a1498beaaea4da47f4121a6122a6d59a37f00e4
GabrielxKuek <kuekguanjiegabriel@gmail.com>
2024-03-15 23:51:55 +0800

    Started working on UI

--- C-Sharp Chess Solution/ChessUI/obj/Debug/net8.0-windows/ChessUI.AssemblyInfo.cs
+++ C-Sharp Chess Solution/ChessUI/obj/Debug/net8.0-windows/ChessUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4085b7013ffe22790f1fac8f551093b06bdefe8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b69c30751d91b3891078fb6e766485093c4c35aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
